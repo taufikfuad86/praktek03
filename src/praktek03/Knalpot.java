0a9775a308530750656fa8f0d553850a594c0f34
taufiqfuad <taufikfuadditlantas@gmail.com>
2018-05-04 15:14:17 +0800

    buat Getter and Setter

--- src/praktek03/Knalpot.java
+++ src/praktek03/Knalpot.java
@@ -10,4 +10,29 @@ public class Knalpot {
                 "Warna \t: "+warna+"\n"+
                 "Harga \t: "+harga);
     }
+
+    public String getMerek() {
+        return merek;
+    }
+
+    public void setMerek(String merek) {
+        this.merek = merek;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
 }
