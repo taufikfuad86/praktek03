115ae5e8a550068c3aa39d5def26314fae13a62e
taufiqfuad <taufikfuadditlantas@gmail.com>
2018-05-04 15:07:07 +0800

    Buat method Cetak Info

--- src/praktek03/Knalpot.java
+++ src/praktek03/Knalpot.java
@@ -4,4 +4,10 @@ public class Knalpot {
     String merek;
     String warna;
     double harga;
+    
+    void cetakinfo(){
+        System.out.println("Merek \t: "+merek+"\n"+
+                "Warna \t: "+warna+"\n"+
+                "Harga \t: "+harga);
+    }
 }
