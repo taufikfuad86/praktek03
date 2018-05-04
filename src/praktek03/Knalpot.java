4910e7f04a6d145209f24d14fd4d270e8fff1af3
taufiqfuad <taufikfuadditlantas@gmail.com>
2018-05-04 15:02:17 +0800

    Membuat Atribut

--- src/praktek03/Knalpot.java
+++ src/praktek03/Knalpot.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek03;
 
-/**
- *
- * @author WAHYU'S PC
- */
 public class Knalpot {
-    
+    String merek;
+    String warna;
+    double harga;
 }
