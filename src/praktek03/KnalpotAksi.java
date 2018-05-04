c7dae21b5e5b40187390ea05e9c5c5e7c3c3056b
taufiqfuad <taufikfuadditlantas@gmail.com>
2018-05-04 15:11:41 +0800

    Instance, buat atribut dan panggil method

--- src/praktek03/KnalpotAksi.java
+++ src/praktek03/KnalpotAksi.java
@@ -1,6 +1,11 @@
 package praktek03;
 public class KnalpotAksi {
     public static void main(String[] args) {
+        Knalpot k1 = new Knalpot();
+        k1.merek ="Purbo";
+        k1.warna ="Perak";
+        k1.harga =500;
         
+        k1.cetakinfo();
     }
 }
