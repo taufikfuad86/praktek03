0b36002d1ea6f4a96995322af0e5bed248c0595d
taufiqfuad <taufikfuadditlantas@gmail.com>
2018-05-04 15:18:32 +0800

    menggunakan method get

--- src/praktek03/KnalpotAksi.java
+++ src/praktek03/KnalpotAksi.java
@@ -7,5 +7,12 @@ public class KnalpotAksi {
         k1.setHarga(500);
         
         k1.cetakinfo();
+        
+        System.out.println("Mereknya \t: ");
+        System.out.println(k1.getMerek());
+        System.out.println("Warnanya \t: ");
+        System.out.println(k1.getWarna());
+        System.out.println("Harganya \t: ");
+        System.out.println(k1.getHarga());
     }
 }
