b29f24ca57be8995e802136f172a9c776ce7e8ab
taufiqfuad <taufikfuadditlantas@gmail.com>
2018-05-04 15:15:38 +0800

    menggunakan method set

--- src/praktek03/KnalpotAksi.java
+++ src/praktek03/KnalpotAksi.java
@@ -2,9 +2,9 @@ package praktek03;
 public class KnalpotAksi {
     public static void main(String[] args) {
         Knalpot k1 = new Knalpot();
-        k1.merek ="Purbo";
-        k1.warna ="Perak";
-        k1.harga =500;
+        k1.setMerek("Purbo");
+        k1.setWarna("Perak");
+        k1.setHarga(500);
         
         k1.cetakinfo();
     }
